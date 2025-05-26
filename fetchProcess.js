f1eebaf70d6eeecc43ae0960c54b40dfc1f96301
techprosignature <phineas.mazzeo@icloud.com>
2025-05-26 07:57:34 -0600

    Add api

--- fetchProcess.js
+++ fetchProcess.js
@@ -1,23 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-const { JSDOM } = require('jsdom');
-
-const URL = "https://www.churchofjesuschrist.org/media/music?lang=eng"; // Replace with the actual URL
 const OUTPUT_FILE = path.join('sacredmusic','lastScript.js');
 
-async function fetchAndExtract() {
+async function processHTMLlibrary(url) {
     try {
-        const response = await globalThis.fetch(URL);
+        const response = await globalThis.fetch(url);
         const html = await response.text();
 
-        const dom = new JSDOM(html);
-        const scripts = [...dom.window.document.querySelectorAll('script')];
+        const JsonText = html.split('window.renderData=')[1]?.split('</script>')[0];
 
-        const lastScript = scripts.length ? scripts[scripts.length - 4].textContent : null;
-        const mainJson = lastScript.split('window.renderData=')[1];
+        if (JsonText) {
+            fs.writeFileSync(path.join('sacredmusic', 'main.json'), JsonText, 'utf8');
+            const parsedJSON = JSON.parse(JsonText);
 
-        if (mainJson) {
-            fs.writeFileSync(path.join('sacredmusic', 'main.json'), mainJson, 'utf8');
+            processEntry(parsedJSON.data.libraryData);
 
             console.log(`Saved last script to ${OUTPUT_FILE}`);
         } else {
@@ -28,4 +24,20 @@ async function fetchAndExtract() {
     }
 }
 
-fetchAndExtract();
+async function processEntry(entryJson){
+    console.log(entryJson['$model'], " ", entryJson.title, " ", entryJson.slug);
+    if(entryJson['$model'] !== 'musicLibraryItem'){
+        for(const entry of entryJson.entries){
+            processEntry(entry);
+        }
+    }
+    else{
+        const response = await (await globalThis.fetch(`https://www.churchofjesuschrist.org/media/music/api?type=songsFilteredList&lang=eng&identifier=%7B%22lang%22%3A%22eng%22%2C%22limit%22%3A500%2C%22offset%22%3A0%2C%22orderByKey%22%3A%5B%22bookSongPosition%22%5D%2C%22bookQueryList%22%3A%5B%22${entryJson.slug}%22%5D%7D&batchSize=20`)).json();
+        fs.writeFileSync(path.join('sacredmusic', 'api', `${entryJson.slug}.json`), JSON.stringify(response.data, null, 2), 'utf8');
+        for(const song of response.data){
+            console.log(song.title, " ", song.bookSectionTitle, " ", song.slug);
+        }
+    }
+}
+
+processHTMLlibrary("https://www.churchofjesuschrist.org/media/music?lang=eng");
